886d0df27b7c51b39e2dc559f77c3610926cc85d
nevz <75290989+nevz9@users.noreply.github.com>
2022-02-27 22:58:13 +0800

    Finish project

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # memory-game
 
-Personal Project.... 
-still in progress
+Just a random personal project made with HTML, CSS, and Javascript.
+
+### Preview
+
+![](preview.png)
+
+### Links
+
+- Live Demo: [https://nevz9.github.io/memory-game/](https://nevz9.github.io/memory-game/)
+
+### Built with
+
+- HTML5 and CSS3
+- Javascript DOM Manipulation
+- Flexbox
+- CSS Grid
+
+### What I've learned
+
+- IIFE Module Pattern
+- DOM Manipulation
+- More about <i><b>this</b></i> (hopefully)
+- Fisher-Yates Shuffle
+
+###
+
+This definitely needs a refactor :(
--- js/script.js
+++ js/script.js
@@ -33,6 +33,28 @@
       this.shuffledItems = [];
     },
 
+    initializeHelpModal() {
+      const helpModal = document.querySelector(".help-modal");
+      const buttonPlay = document.querySelector(".btn--play");
+      helpModal.style.display = "flex";
+      buttonPlay.addEventListener("click", () => {
+        helpModal.style.display = "none";
+        this.startTime();
+      });
+    },
+
+    initializeEndModal() {
+      const endModal = document.querySelector(".end-modal");
+      const buttonReset = document.querySelector(".btn--reset");
+
+      endModal.style.display = "flex";
+
+      buttonReset.addEventListener("click", () => {
+        endModal.style.display = "none";
+        window.location.reload();
+      });
+    },
+
     shuffleItems() {
       for (let i = this.letterArr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * i + 1);
@@ -42,74 +64,129 @@
           this.letterArr[i],
         ];
       }
-
       this.shuffledItems.push(this.letterArr);
     },
 
-    createGrid(n) {
+    createGrid() {
       this.gridContainer = document.createElement("div");
       this.gridContainer.classList.add("grid-container");
       document.body.prepend(this.gridContainer);
-      for (let i = 0; i < n; i++) {
+      for (let i = 0; i < 24; i++) {
         this.item = document.createElement("div");
+        this.itemInner = document.createElement("div");
         this.itemOverlay = document.createElement("div");
         this.itemText = document.createElement("p");
         this.item.classList.add("grid-item");
+        this.itemInner.classList.add("grid-item-inner");
         this.itemOverlay.classList.add("grid-item-overlay");
         this.itemText.classList.add("grid-item-text");
-        this.item.dataset.index = i;
-        this.item.dataset.clicked = false;
+        this.itemOverlay.textContent = "?";
+        this.itemInner.dataset.index = i;
+        this.itemOverlay.dataset.index = i;
+        this.itemInner.dataset.clicked = false;
         this.gridContainer.appendChild(this.item);
-        this.item.appendChild(this.itemOverlay);
-        this.item.appendChild(this.itemText);
+        this.item.appendChild(this.itemInner);
+        this.itemInner.appendChild(this.itemOverlay);
+        this.itemInner.appendChild(this.itemText);
       }
-      this.gridItem = document.querySelectorAll(".grid-item");
+      this.itemInner = document.querySelectorAll(".grid-item-inner");
     },
 
     addText() {
-      for (let i = 0; i < this.gridItem.length; i++) {
-        this.gridItem[i].children[1].textContent =
+      for (let i = 0; i < this.itemInner.length; i++) {
+        this.itemInner[i].children[1].textContent =
           this.shuffledItems[0][i].toUpperCase();
       }
     },
 
     removeOverlay() {
-      this.gridItem.forEach((item) => {
+      this.itemInner.forEach((item) => {
         item.classList.remove("grid-item-flip");
         this.counter = 0;
       });
     },
 
     checkGuess() {
-      const filteredItem = [...this.gridItem].filter((item) =>
+      const filteredItem = [...this.itemInner].filter((item) =>
         this.itemIndex.includes(item.dataset.index)
       );
+
       for (let i = 0; i < filteredItem.length; i++) {
         if (this.guessArr[0] === this.guessArr[1]) {
           filteredItem[i].dataset.clicked = false;
           filteredItem[i].dataset.found = "true";
           setTimeout(() => {
-            filteredItem[i].style.background = "green";
+            filteredItem[i].children[1].style.background = "green";
           }, 500);
         }
         filteredItem[i].dataset.clicked = false;
       }
     },
 
-    checkAllFound() {
-      const isFound = [...this.gridItem].every(
+    startTime() {
+      this.start = window.performance.now();
+    },
+
+    checkEveryItem() {
+      const isEveryItemFound = [...this.itemInner].every(
         (item) => item.dataset.found === "true"
       );
-      console.log([...this.gridItem]);
-      if (isFound) {
+
+      if (isEveryItemFound) {
         setTimeout(() => {
-          alert("good job!");
+          this.initializeEndModal();
+          this.end = window.performance.now();
+          this.time = this.end - this.start;
+          this.getCurrentTime();
+          this.getBestTime();
         }, 200);
       }
     },
 
+    getCurrentTime() {
+      const currentTime = document.querySelector(".time");
+      if (!this.getLocalStorage()) {
+        this.setLocalStorage(this.time);
+      }
+      if (this.getLocalStorage() > this.time) {
+        this.setLocalStorage(this.time);
+      }
+
+      if (this.time > 60000) {
+        const minutes = (this.time / 1000 / 60).toFixed(2).slice(0, 1);
+        const seconds = (this.time / 1000 / 60).toFixed(2).slice(2);
+        currentTime.textContent = `${minutes} minutes and ${seconds} seconds`;
+      } else {
+        currentTime.textContent = `${(this.time / 1000).toFixed(2)} seconds`;
+      }
+    },
+
+    getBestTime() {
+      const bestTime = document.querySelector(".best-time");
+      const minutes = (this.getLocalStorage() / 1000 / 60)
+        .toFixed(2)
+        .slice(0, 1);
+      const seconds = (this.getLocalStorage() / 1000 / 60).toFixed(2).slice(2);
+
+      if (this.getLocalStorage() > 60000) {
+        bestTime.textContent = `${minutes} minutes and ${seconds} seconds`;
+      } else {
+        bestTime.textContent = `${(this.getLocalStorage() / 1000).toFixed(
+          2
+        )} seconds`;
+      }
+    },
+
+    setLocalStorage(n) {
+      localStorage.setItem("best-time", n);
+    },
+
+    getLocalStorage() {
+      return localStorage.getItem("best-time");
+    },
+
     clickListener() {
-      this.gridItem.forEach((item) =>
+      this.itemInner.forEach((item) =>
         item.addEventListener("click", () => {
           if (
             this.counter < 2 &&
@@ -117,17 +194,17 @@
             item.dataset.clicked === "false"
           ) {
             item.classList.add("grid-item-flip");
+            item.dataset.clicked = true;
             this.guessArr.push(item.children[1].textContent);
             this.itemIndex.push(item.dataset.index);
-            item.dataset.clicked = true;
             this.counter += 1;
           }
           if (this.counter >= 2) {
             this.checkGuess();
-            this.checkAllFound();
+            this.checkEveryItem();
             setTimeout(() => {
               this.removeOverlay();
-            }, 500);
+            }, 800);
             this.guessArr = [];
             this.itemIndex = [];
           }
@@ -135,9 +212,10 @@
       );
     },
   };
+  Game.initializeHelpModal();
   Game.utilities();
   Game.shuffleItems();
-  Game.createGrid(24);
+  Game.createGrid();
   Game.addText();
   Game.clickListener();
 })();
